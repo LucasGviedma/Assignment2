6e84267697b612d6d66be8923826ce87ff68adc0
Lucas García de Viedma Pérez <47993625+LucasGviedma@users.noreply.github.com>
2022-03-15 18:57:53 +0100

    fix

--- Assignment3/text.py
+++ Assignment3/text.py
@@ -1,47 +1,65 @@
 # import the necessary packages
-from scipy.spatial import distance as dist
-from matplotlib import pyplot as plt
-import numpy as np
-import argparse
 import glob
 import cv2
+import scipy.spatial.distance as dist
+import numpy as np
 from skimage.io import imread
-from skimage.transform import resize
-from skimage.feature import hog
-from sklearn import svm
-from skimage import exposure
-
-images = {}
-
-hog_images = []
-hog_featur = []
-
-# AVG SIZES -> x = 2398.44 || y = 721.34
-
-# loop over the image paths
-for imagePath in glob.glob("rice_leaf_diseases\\/*\\" + "/*.JPG"):
-
-	image = imread(imagePath)
- 
-	filename = str(imagePath.split(sep="\\")[2])
-	images[filename] = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
- 
-	resized_img = resize(image, (721.34, 2398.44))
-	hog_feature, hog_image = hog(resized_img, orientations=9, pixels_per_cell=(8, 8),
-                        cells_per_block=(2, 2), visualize=True, multichannel=True)
- 
-	hog_images.append(hog_image)
-	hog_featur.append(hog_feature)
 
 
-clf = svm.SVC()
-hog_featur = np.array(hog_featur)
-np.random.shuffle(hog_featur)
+def getIMG(imagePath):
+    image = imread(imagePath)
+    image = cv2.resize(image, (2398, 721))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
 
-percentage = 80
-partition = int(len(hog_featur)*percentage/100)
 
-x_train, x_test = hog_featur[:partition,:-1],  hog_featur[partition:,:-1]
-y_train, y_test = hog_featur[:partition,-1:].ravel() , hog_featur[partition:,-1:].ravel()
+def getHOG(image):
+	image_hog = cv2.HOGDescriptor().compute(image, (32, 32))
+	image_hog = cv2.normalize(image_hog, image_hog).flatten()
+	return image_hog
 
-clf.fit(x_train,y_train)
+
+def getHCL(image):
+	image_hcl = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+	image_hcl = cv2.normalize(image_hcl, image_hcl).flatten()
+	return image_hcl
+
+
+# Read chosen image
+chosen_image     = getIMG("rice_leaf_diseases/leaf smut/leaf_smut_6.jpg")
+chosen_image_hog = getHOG(chosen_image)
+chosen_image_hcl = getHCL(chosen_image)
+
+# Read comparison images
+images     = []
+images_nam = []
+images_hog = []
+images_hcl = []
+
+for image_path in glob.glob("rice_leaf_diseases\\/*\\" + "/*.JPG"):
+    
+    image = getIMG(image_path)
+    images.append(image)
+    images_hog.append(getHOG(image))
+    images_nam.append(str(image_path.split(sep="\\")[2]))
+    
+    #hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    #new_values = [[pixel if pixel < 225 else 0 for pixel in row] for row in hsv_image[:,:,2]]
+    #hsv_image[:,:,2] = new_values
+
+distances_hog = []
+distances_hcl = []
+
+for image_hog in images_hog:
+    distances_hog.append(dist.euclidean(image_hog, chosen_image_hog))
+most_similar_hogs = np.argsort(distances_hog)[:15]
+
+for i in np.array(images)[most_similar_hogs]:
+	images_hcl.append(getHCL(image))
+
+for image_hcl in images_hcl:
+    distances_hcl.append(dist.euclidean(image_hcl, chosen_image_hcl))
+most_similar_hcls = np.argsort(distances_hcl)[:5]
+
+most_similar_imgs = np.array(most_similar_hogs)[most_similar_hcls]
+print(np.array(images_nam)[most_similar_imgs])
