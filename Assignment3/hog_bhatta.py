ad1b5fff2a689fd1f368898331e64857aeb9b31d
LuciaCatalan <55504760+LuciaCatalan@users.noreply.github.com>
2022-03-28 22:56:42 +0200

    Update hog_bhatta.py

--- Assignment3/hog_bhatta.py
+++ Assignment3/hog_bhatta.py
@@ -3,8 +3,11 @@ CBIR HOG and Bhattacharyya distance
 Input: path to an image
 Output: 5 most similar images
 '''
+# INPUT IMAGE
+# ej: rice_leaf_diseases_no_back/leaf smut/leaf_smut_6.jpg
+image_path = "rice_leaf_diseases_no_back/leaf smut/leaf_smut_6.jpg"
 
-# import the necessary packages
+# Import the necessary packages
 import glob
 import cv2
 import numpy as np
@@ -27,43 +30,26 @@ def getHOG(image):
 	image_hog = cv2.normalize(image_hog, image_hog).flatten()
 	return image_hog
 
-
-def main():
+# Read chosen image
+chosen_image     = getIMG(image_path)
+# Compute HOG of the image
+chosen_image_hog = getHOG(chosen_image)
     
-    #Input image
-    #ej: rice_leaf_diseases_no_back/leaf smut/leaf_smut_6.jpg (without "")
-    image_path = input("Enter image path:")
+# Read comparison images
+images_hog  = []
+images_name = []
     
-    # Choose if the images' dataset (y is fixed background)
-    choose_back = input("Want to use the database with fixed background? (y/n):")
-    if choose_back == "y":
-        background = "rice_leaf_diseases\\/*\\"
-    else:
-        background = "rice_leaf_diseases_no_back\\/*\\"
-
-    # Read chosen image
-    chosen_image     = getIMG(image_path)
-    # Compute HOG of the image
-    chosen_image_hog = getHOG(chosen_image)
+for image_path in glob.glob("rice_leaf_diseases_no_back\\/*\\" + "/*.JPG"):
+    image = getIMG(image_path)
+    images_hog.append(getHOG(image))
+    images_name.append(str(image_path.split(sep="\\")[2]))
     
-    # Read comparison images
-    images_hog  = []
-    images_name = []
+distances_hog = []
+# Compare the distance of the hog images
+for image_hog in images_hog:
+    distances_hog.append(cv2.compareHist(image_hog, chosen_image_hog, cv2.HISTCMP_BHATTACHARYYA))
     
-    for image_path in glob.glob(background + "/*.JPG"):
-        image = getIMG(image_path)
-        images_hog.append(getHOG(image))
-        images_name.append(str(image_path.split(sep="\\")[2]))
-    
-    distances_hog = []
-    # Compare the distance of the hog images
-    for image_hog in images_hog:
-        distances_hog.append(cv2.compareHist(image_hog, chosen_image_hog, cv2.HISTCMP_BHATTACHARYYA))
-    
-    # Print the 5 images with the lower distance      
-    most_similar_hogs = np.argsort(distances_hog)[:5]
-    print("The five most similar images are: \n")
-    print(np.array(images_name)[most_similar_hogs])
-
-if __name__ == '__main__':
-    main()
+# Print the 5 images with the lower distance      
+most_similar_hogs = np.argsort(distances_hog)[:5]
+print("The five most similar images are: \n")
+print(np.array(images_name)[most_similar_hogs])
